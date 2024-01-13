1b861c759d44cab2f7714c3e7577d7021fd1b93f
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2024-01-13 20:53:43 +0200

    I've made small malware

--- 1. Application Programs/Browser_PopUp_Virus/Browser_PopUp_Virus.py
+++ 1. Application Programs/Browser_PopUp_Virus/Browser_PopUp_Virus.py
@@ -1,13 +1,5 @@
 import os
-import time
-counter = 0
-timer = 60
 
-while counter < 3:
-    counter += 1
-    time.sleep(timer)
-    bomb = 'microsoftedge /new-window https://thriving-paletas-d5335c.netlify.app/'
-    os.system(bomb)
-    timer -= 29
-else:
-    os.system("shutdown /s")
+edge_path = r'"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"'
+os.system(f'{edge_path} /new-window https://thriving-paletas-d5335c.netlify.app/')
+#os.system("shutdown /s") #DANGER!
